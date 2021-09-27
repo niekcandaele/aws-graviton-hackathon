8051a1e7f328feaf7a3565f85856b479eae476a0
Niek Candaele <niek@candaele.dev>
2021-09-27 18:27:01 +0200

    only get 10 matches at once

--- faceit-scraper/lib/getHub.mjs
+++ faceit-scraper/lib/getHub.mjs
@@ -1,6 +1,6 @@
 import { faceitRequest } from './faceitRequest.mjs';
 
 export async function getMatchesForHub(hubId) {
-  const response = await faceitRequest(`/hubs/${hubId}/matches?type=past&offset=0&limit=20`)
+  const response = await faceitRequest(`/hubs/${hubId}/matches?type=past&offset=0&limit=10`)
   return response.data
 }
