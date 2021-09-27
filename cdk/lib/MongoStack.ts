c89f2be7df0f848c97ade3e06e500d4596cc4b9b
Niek Candaele <niek@candaele.dev>
2021-09-27 20:42:49 +0200

    add an ec2 instance to ssh tunnel to mongo

--- cdk/lib/MongoStack.ts
+++ cdk/lib/MongoStack.ts
@@ -1,6 +1,7 @@
 import { ClusterParameterGroup, DatabaseCluster } from '@aws-cdk/aws-docdb';
 import * as ec2 from '@aws-cdk/aws-ec2';
-import { Vpc } from '@aws-cdk/aws-ec2';
+import { AmazonLinuxCpuType, BastionHostLinux, Peer, Vpc } from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
 import * as cdk from '@aws-cdk/core';
 import { Construct } from '@aws-cdk/core';
 
@@ -36,5 +37,41 @@ export class Mongo extends Construct{
     });
 
     databaseCluster.connections.allowDefaultPortFromAnyIpv4();
+
+    const sshServerSG = new ec2.SecurityGroup(this, 'webserver-sg', {
+      vpc,
+      allowAllOutbound: true,
+    });
+
+    sshServerSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'allow SSH access from anywhere',
+    );
+
+    const sshServerRole = new iam.Role(this, 'sshserver-role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDocDBFullAccess'),
+      ],
+    });
+
+    const ec2Instance = new ec2.Instance(this, 'ec2-instance', {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      role: sshServerRole,
+      securityGroup: sshServerSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      keyName: 'cata',
+    });
+
   }
 }
