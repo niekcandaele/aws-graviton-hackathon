df7fc8ab47633287b828308f2e582f94ffacc54a
Niek Candaele <niek@candaele.dev>
2021-09-27 18:34:29 +0200

    change schedule of faceit scraper

--- cdk/lib/faceitScraper.ts
+++ cdk/lib/faceitScraper.ts
@@ -47,7 +47,7 @@ export class FaceitScraper extends Construct {
           MONGODB_URI: process.env.MONGODB_URI
         }
       },
-      schedule: Schedule.expression('rate(30 minutes)'),
+      schedule: Schedule.expression('rate(4 hours)'),
       platformVersion: ecs.FargatePlatformVersion.LATEST,
     });
 
