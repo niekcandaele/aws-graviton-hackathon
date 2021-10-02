70d06c4cec956fce145396b89e075d1bfaa83f8e
Niek Candaele <niek@candaele.dev>
2021-10-02 18:49:12 +0200

    Stats page (the API doesnt actually calculate a lot right now, so it's kinda barebones)

--- frontend/src/pages/stats.tsx
+++ frontend/src/pages/stats.tsx
@@ -1,8 +1,29 @@
+import { Button, Col, Row, Statistic } from 'antd';
+import { useEffect, useState } from 'react';
+
+import { get } from '../../lib/http';
+
+interface IStats {
+  totalMatches: number
+}
+
 export default function Stats() {
-  console.log('stats')
+  const [stats, setStats] = useState<IStats>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await get('/stats');
+      setStats(response.data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Stats</h1>
+          <Statistic title="Total matches" value={loading ? 0 : stats.totalMatches} loading={loading} />
     </div>
-  )
+  );
 }
