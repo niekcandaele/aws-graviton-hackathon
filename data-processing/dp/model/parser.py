b0c3d4b8d6700dd50329549f4c7ba83eb370d103
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-10-13 20:14:01 +0200

    Add multiple new features + retrain model

--- data-processing/dp/model/parser.py
+++ data-processing/dp/model/parser.py
@@ -1,8 +1,8 @@
 from abc import abstractclassmethod
 
 from numpy import nan
-from dp.database.connection import get_collections
-from dp.stats.dynamo
+from dp.database.mongo import get_collections
+from dp.database.dynamo import getItem
 from bson.objectid import ObjectId
 from pymongo import ASCENDING
 from dataclasses import make_dataclass
@@ -15,52 +15,115 @@ pi = player info
 collections = get_collections()
 
 
-def get_map_win_rate():
-    return 0
+def get_bomb_plant_event(round_id):
+    round_details = collections["rounds"].find_one({"_id": round_id})
+    if "bombStatusChanges" not in round_details:
+        return None
+
+    bomb_event_ids = round_details["bombStatusChanges"]
+    if len(bomb_event_ids) == 0:
+        return None
+
+    bomb_planted_event = collections["bombstatuschanges"].find_one(
+        {"_id": {"$in": bomb_event_ids}, "status": "PLANTED"}
+    )
+    return bomb_planted_event
 
 
 def preprocessing(data):
     # One hot encoding (first blood)
-    return data.join(pd.get_dummies(data.first_blood, prefix="first_blood_", dummy_na=True)).drop("first_blood", axis=1)
+    return data.join(
+        pd.get_dummies(data.first_blood, prefix="first_blood_", dummy_na=True)
+    ).drop("first_blood", axis=1)
 
 
 class Team:
-
     def __init__(self, team_id):
         self.id = team_id
-        team = collections["teams"].find_one({"_id": ObjectId(team_id)}, {"players": 1, "_id": 0, "startingSide": 1})
+        team = collections["teams"].find_one(
+            {"_id": ObjectId(team_id)}, {"players": 1, "_id": 0, "startingSide": 1}
+        )
         self.player_ids = team["players"]
         self.starting_side = team["startingSide"]
 
 
 class Round:
-    def __init__(self, round_id, team_id, max_tick, round_number):
+    def __init__(self, round_id, team_id, max_tick, round_number, map):
         self.round_id = round_id
         self.team = Team(team_id)
+        self.map = map
 
         # Parse all data that is less than this tick.
         self.max_tick = max_tick
         self.round_number = round_number
 
+        # Get basic round information here. Instead of getting the round information over and over again.
+        round_details = collections["rounds"].find_one({"_id": round_id})
+        self.player_hurt_ids = round_details["playerHurts"]
+        self.bomb_plant_tick = None
+
     @abstractclassmethod
     def dataclass(self):
-        return make_dataclass("Round", [
-            ("kills", int),
-            ("deaths", int),
-            ("first_blood", int),
-            ("round_winstreak", int),
-            ("bomb_planted", int),
-            ("is_terrorist", int),
-            ("defuse_kits", int),
-        ])
+        return make_dataclass(
+            "Round",
+            [
+                ("kills", int),
+                ("deaths", int),
+                ("first_blood", int),
+                ("round_winstreak", int),
+                ("bomb_planted", int),
+                ("is_terrorist", int),
+                ("counter_terrorist_map_winrate", float),
+                ("terrorist_map_winrate", float),
+                ("damage_given", int),
+                ("damage_taken", int),
+                ("team_alive_on_plant", int),
+                ("enemy_alive_on_plant", int),
+            ],
+        )
+
+    def map_winrate(self):
+        winrate_details = getItem(f"mapwinrate_{self.map}")["data"]
+        ct_wins = winrate_details["COUNTER_TERRORIST"]
+        t_wins = winrate_details["TERRORIST"]
+        total = ct_wins + t_wins
+
+        ct_winrate = ct_wins / total
+        t_winrate = t_wins / total
+        return (ct_winrate, t_winrate)
+
+    def damage(self):
+        player_hurts = list(
+            collections["playerhurts"].find({"_id": self.player_hurt_ids})
+        )
+        damage_taken = 0
+        damage_given = 0
+
+        for player_hurt in player_hurts:
+            victim_pi = collections["victim"].find_one({"_id": player_hurt["victim"]})
+            if "player" in victim_pi and victim_pi["player"] in self.team.player_ids:
+                damage_taken += player_hurt["healthDmg"]
+            elif "player" in victim_pi:
+                damage_given += player_hurt["healthDmg"]
+        return (damage_given, damage_taken)
 
     def is_bomb_planted(self):
-        bomb_event_ids = collections["rounds"].find({"_id": ObjectId(self.round_id)}, {"bombStatusChanges": 1})["bombStatusChanges"]
+        round_details = collections["rounds"].find_one({"_id": self.round_id})
+        if "bombStatusChanges" not in round_details:
+            return 0
+        bomb_event_ids = round_details["bombStatusChanges"]
         if len(bomb_event_ids) == 0:
-            return False
+            return 0
 
-        bomb_planted_event = list(collections["bombstatusevents"].find({"_id": {"$in": bomb_event_ids}}, {"status": 1}))
-        return 1 if len(bomb_planted_event) else 0
+        bomb_planted_event = collections["bombstatuschanges"].find_one(
+            {"_id": {"$in": bomb_event_ids}, "status": "PLANTED"}, {"tick": 1}
+        )
+
+        if bomb_planted_event is not None:
+            self.bomb_plant_tick = bomb_planted_event["tick"]
+            return 1
+        else:
+            return 0
 
     def is_terrorist(self):
         if self.team.starting_side == "TERRORIST":
@@ -68,21 +131,31 @@ class Round:
         else:
             return 0 if self.round_number < 16 else 1
 
-    def kills_and_deaths(self):
+    def kills_and_deaths(self, tick=None):
+
+        if tick is None:
+            tick = self.max_tick
+
         death_count = kill_count = 0
 
-        round_details = collections["rounds"].find_one({"_id": ObjectId(self.round_id)}, {"kills": 1})
+        round_details = collections["rounds"].find_one(
+            {"_id": ObjectId(self.round_id)}, {"kills": 1}
+        )
         if "kills" not in round_details:
             return (0, 0)
 
         for kill_id in list(round_details["kills"]):
 
-            kill = collections["playerkills"].find_one({"_id": kill_id, "tick": {"$lte": self.max_tick}}, {"victim": 1})
+            kill = collections["playerkills"].find_one(
+                {"_id": kill_id, "tick": {"$lte": tick}}, {"victim": 1}
+            )
             if kill is None or "victim" not in kill:
                 continue
 
             # Sometimes a playerInfo does not contain a player field. We just ignore that case.
-            victim_pi = collections["playerinfos"].find_one({"_id": kill["victim"]}, {"player": 1})
+            victim_pi = collections["playerinfos"].find_one(
+                {"_id": kill["victim"]}, {"player": 1}
+            )
             if "player" not in victim_pi:
                 continue
 
@@ -94,18 +167,27 @@ class Round:
         return (kill_count, death_count)
 
     def is_first_blood(self):
-        kill_ids = collections["rounds"].find_one({"_id": ObjectId(self.round_id)}, {"kills": 1})["kills"]
+        kill_ids = collections["rounds"].find_one(
+            {"_id": ObjectId(self.round_id)}, {"kills": 1}
+        )["kills"]
 
         # Get the player info from the first kill based on lowest tick.
-        victim_pi_id = list(collections["playerkills"]
-                            .find({"_id": {"$in": kill_ids}, "tick": {"$lte": self.max_tick}}, {"victim": 1, "tick": 1})  # get kills
-                            .sort([("tick", ASCENDING)])                                                                  # order from low to high
-                            .limit(1))
+        victim_pi_id = list(
+            collections["playerkills"]
+            .find(
+                {"_id": {"$in": kill_ids}, "tick": {"$lte": self.max_tick}},
+                {"victim": 1, "tick": 1},
+            )  # get kills
+            .sort([("tick", ASCENDING)])  # order from low to high
+            .limit(1)
+        )
 
         if len(victim_pi_id) == 0:
             return nan
 
-        victim_pi = collections["playerinfos"].find_one({"_id": ObjectId(victim_pi_id[0]["victim"])}, {"player": 1})
+        victim_pi = collections["playerinfos"].find_one(
+            {"_id": ObjectId(victim_pi_id[0]["victim"])}, {"player": 1}
+        )
 
         # We just ignore this case
         if "player" not in victim_pi:
@@ -115,16 +197,22 @@ class Round:
         return 0 if victim_pi["player"] in self.team.player_ids else 1
 
     def is_win(self):
-        return 0 if collections["rounds"].find_one({"_id": self.round_id}, {"winningTeam": 1})["winningTeam"] == self.team.id else 1
+        return (
+            0
+            if collections["rounds"].find_one(
+                {"_id": self.round_id}, {"winningTeam": 1}
+            )["winningTeam"]
+            == self.team.id
+            else 1
+        )
 
     def equipment_value_ratio():
         # TODO:
         return True
 
-    def defuse_kit_count():
-        # TODO:
-        return 5
-
-    def alive_ratio_on_bomb_plant():
-        # TODO:
-        return 0
+    def players_alive_on_plant(self):
+        MAX_PLAYERS = 5
+        if self.bomb_plant_tick is None:
+            return (0, 0)
+        (kills, deaths) = self.kills_and_deaths(self.bomb_plant_tick)
+        return (MAX_PLAYERS - deaths, MAX_PLAYERS - kills)
--- data-processing/dp/model/predict.py
+++ data-processing/dp/model/predict.py
@@ -1,50 +1,117 @@
-from ..database.connection import get_collections
-from .parser import Round, preprocessing
+from dp.database.mongo import get_collections
+from dp.model.parser import Round, preprocessing, get_bomb_plant_event
 import pickle
 import pandas as pd
-
-import os
 
 
 def predict():
 
     try:
         collections = get_collections()
-        print(os.getcwd())
-        file = open("./dp/model/model.sav", 'rb')
+        file = open("./dp/model/model.sav", "rb")
         model = pickle.load(file)
 
         round_dataclass = Round.dataclass()
-        rounds = list(collections["rounds"].find({"has_predictions": {"$exists": False}}))
+        rounds = list(
+            collections["rounds"].find({"has_predictions": {"$exists": False}})
+        )
 
         for index, round in enumerate(rounds):
+            round_id = round["_id"]
             print(f"{index}/{len(rounds)} rounds")
 
-            # The teamids are saved in the match, we need this tho :(
-            match = list(collections["matches"].find({"rounds": {"$elemMatch": {"$eq": round["_id"]}}}))[0]
+            # The teamids are saved in the match, we need this though :)
+            match = list(
+                collections["matches"].find(
+                    {"rounds": {"$elemMatch": {"$eq": round_id}}}
+                )
+            )[0]
+
+            # this is a list of all the events we create a prediction for
+            predict_events = []
 
             # Get ticks from all kills
-            kills = list(collections["playerkills"].find({"_id": {"$in": round["kills"]}}, {"_id": 1, "tick": 1}))
-
-            for kill in kills:
-                parsed_round = Round(round["_id"], match["teams"][0], kill["tick"])
-
-                (kills, deaths) = parsed_round.kills_and_deaths()
-                data = pd.DataFrame([round_dataclass(
-                    kills=kills,
-                    deaths=deaths,
-                    first_blood=parsed_round.is_first_blood(),
-                    round_winstreak=0,
-                    bomb_planted=parsed_round.is_bomb_planted(),
-                    is_terrorist=parsed_round.is_terrorist(),
+            kills = list(
+                collections["playerkills"].find(
+                    {"_id": {"$in": round["kills"]}}, {"_id": 1, "tick": 1}
                 )
-                ])
+            )
+
+            # All kill events
+            for kill in kills:
+                predict_event.append(
+                    {
+                        "id": kill["_id"],
+                        "collection": "playerkills",
+                        "tick": kill["tick"],
+                    }
+                )
+
+            if "bombStatusChanges" in round:
+                bomb_events = list(
+                    collections["bombstatuschanges"].find(
+                        {"_id": {"$in": round["bombStatusChanges"]}},
+                        {"_id": 1, "tick": 1},
+                    )
+                )
+                for bomb_event in bomb_events:
+                    predict_events.append(
+                        {
+                            "id": bomb_event["_id"],
+                            "collection": "bombstatuschanges",
+                            "tick": bomb_event["tick"],
+                        }
+                    )
+
+            for predict_event in predict_events:
+                parsed_round = Round(
+                    round["_id"], match["teams"][0], predict_event["tick"]
+                )
+                (kills, deaths) = parsed_round.kills_and_deaths()
+                (ct_winrate, t_winrate) = parsed_round.map_winrate()
+                (damage_given, damage_taken) = round.damage()
+                # is_bomb_planted() needs to be handles before
+                # players_alive_on_plant(), it depends on property set in is_bomb_planted()
+                is_bomb_planted = round.is_bomb_planted()
+                (
+                    team_alive_on_plant,
+                    enemy_alive_on_plant,
+                ) = round.players_alive_on_plant()
+
+                data = pd.DataFrame(
+                    [
+                        round_dataclass(
+                            kills=kills,
+                            deaths=deaths,
+                            first_blood=parsed_round.is_first_blood(),
+                            round_winstreak=0,
+                            bomb_planted=is_bomb_planted,
+                            is_terrorist=parsed_round.is_terrorist(),
+                            counter_terrorist_map_winrate=ct_winrate,
+                            terrorist_map_winrate=t_winrate,
+                            damage_given=damage_given,
+                            damage_taken=damage_taken,
+                            team_alive_on_plant=team_alive_on_plant,
+                            enemy_alive_on_plant=enemy_alive_on_plant,
+                        )
+                    ]
+                )
 
                 data = preprocessing(data)
-                predicted_winner = match["teams"][0] if model.predict(data) else match["teams"][1]
+                predicted_winner = (
+                    match["teams"][0] if model.predict(data) else match["teams"][1]
+                )
 
-              # TODO: calculate the certainty
-                collections["playerkills"].update_one({"_id": kill["_id"]}, {"$set": {"prediction.team_id": predicted_winner, "prediction.certainty": .8}})
+                # TODO: calculate the certainty
+                collections[predict_event["collection"]].update_one(
+                    {"_id": predict_event["id"]},
+                    {
+                        "$set": {
+                            "prediction.team_id": predicted_winner,
+                            "prediction.certainty": 0.8,
+                        }
+                    },
+                )
 
     except pickle.PickleError:
         print("Could not load model")
